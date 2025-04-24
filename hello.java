f88a761e3c640f478bff0da5e675f4cbd06a7564
Dema-rl <damiano.demartino@capellinisauro.edu.it>
2025-04-24 07:31:32 +0000

    Commit iniziale

--- hello.java
+++ hello.java
@@ -2,6 +2,17 @@ import java.io.*;
 public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello DeMartino");
+
+		//stampo 3 volte il nome 
+		for(int i=0;i<3;i++){
+			System.out.println("DeMartino");
+		}
+
+		//stampo la data di nascita 
+		System.out.println("22/12/2007");
+
+		//stampo il tifo della squadra 
+		System.out.println("Forza Spezia!");
 	}
 }
 
