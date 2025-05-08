94d46803bc1e7dcde7cf2b5c4fb4b9be29e6a45d
Dema-rl <damiano.demartino@capellinisauro.edu.it>
2025-05-08 06:41:22 +0000

    Aggiunta stampa da Array statico

--- hello.java
+++ hello.java
@@ -13,7 +13,30 @@ public class hello {
 
 		//stampo il tifo della squadra 
 		System.out.println("Forza Spezia!");
-	}
+
+		//Array d icaratteri con lettere del nome "DeMartino"
+		char[] nome = {'D','e','M','a','r','t','i','n','o'};
+
+		//Stampo le lettere dell'array 
+		for(char lettera:lettere){
+			System.out.println(lettera + "");
+		}
+
+		Vector<Character> lettere = new Vector<>();
+		lettere.add('D');
+		lettere.add('e');
+		lettere.add('M');
+		lettere.add('a');
+		lettere.add('r');
+		lettere.add('t');
+		lettere.add('i');
+		lettere.add('n');
+		lettere.add('o');
+
+		for(Character lettera:lettere){
+			System.out.println(lettera);
+		}
+	}	
 }
 
 
